eba8d28de73bf59e39be37fc84ff788e6e37cdc5
COSMO ANDRADE FELIX <57240412+CosmoAndrade@users.noreply.github.com>
2022-01-13 16:13:22 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Rotas from "./routes";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Rotas ReactJS</h1>
+      <Rotas />
+    </div>
+  );
+}
